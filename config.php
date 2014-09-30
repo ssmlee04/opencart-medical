6542af488415be9b898fb675dec612a0bd3495a5
Chandler Lee <admin@catfishlady.com>
2014-09-30 11:08:39 +0800

    congih

--- config.php
+++ config.php
@@ -1,24 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/opencart2/');
-define('HTTP_CATALOG', 'http://localhost/opencart2/');
+
+// $NODE_ENV = getenv('NODE_ENV');
+$NODE_ENV = 'opencart2';
+
+define('HTTP_SERVER', 'http://localhost/' . $NODE_ENV . '/');
+define('HTTP_CATALOG', 'http://localhost/' . $NODE_ENV . '/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/opencart2/');
-define('HTTPS_CATALOG', 'http://localhost/opencart2/');
+define('HTTPS_SERVER', 'http://localhost/' . $NODE_ENV . '/');
+define('HTTPS_CATALOG', 'http://localhost/' . $NODE_ENV . '/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/opencart2/');
-define('DIR_SYSTEM', '/var/www/html/opencart2/system/');
-define('DIR_DATABASE', '/var/www/html/opencart2/system/database/');
-define('DIR_LANGUAGE', '/var/www/html/opencart2/language/');
-define('DIR_TEMPLATE', '/var/www/html/opencart2/view/template/');
-define('DIR_CONFIG', '/var/www/html/opencart2/system/config/');
-define('DIR_IMAGE', '/var/www/html/opencart2/image/');
-define('DIR_CACHE', '/var/www/html/opencart2/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/opencart2/download/');
-define('DIR_LOGS', '/var/www/html/opencart2/system/logs/');
-define('DIR_CATALOG', '/var/www/html/opencart2/catalog/');
+define('DIR_APPLICATION', '/var/www/html/' . $NODE_ENV . '/');
+define('DIR_SYSTEM', '/var/www/html/' . $NODE_ENV . '/system/');
+define('DIR_DATABASE', '/var/www/html/' . $NODE_ENV . '/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/' . $NODE_ENV . '/language/');
+define('DIR_TEMPLATE', '/var/www/html/' . $NODE_ENV . '/view/template/');
+define('DIR_CONFIG', '/var/www/html/' . $NODE_ENV . '/system/config/');
+define('DIR_IMAGE', '/var/www/html/' . $NODE_ENV . '/image/');
+define('DIR_CACHE', '/var/www/html/' . $NODE_ENV . '/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/' . $NODE_ENV . '/download/');
+define('DIR_LOGS', '/var/www/html/' . $NODE_ENV . '/system/logs/');
+define('DIR_CATALOG', '/var/www/html/' . $NODE_ENV . '/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
