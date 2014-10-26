d6d080b5b7826caa4d5ab5b443237ab2dd9bc10a
Chandler Lee <admin@catfishlady.com>
2014-10-26 09:54:19 +0800

    x

--- config.php
+++ config.php
@@ -4,6 +4,7 @@
 // $NODE_ENV = getenv('NODE_ENV');
 $NODE_ENV = 'opencart2';
 $WEBSITE_ENV = 'localhost';
+$RELPATH = '/var/www/html';
 
 define('HTTP_SERVER', 'http://' . $WEBSITE_ENV . '/' . $NODE_ENV . '/');
 define('HTTP_CATALOG', 'http://' . $WEBSITE_ENV .'/' . $NODE_ENV . '/');
@@ -13,17 +14,17 @@ define('HTTPS_SERVER', 'http://' . $WEBSITE_ENV . '/' . $NODE_ENV . '/');
 define('HTTPS_CATALOG', 'http://' . $WEBSITE_ENV . '/' . $NODE_ENV . '/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/' . $NODE_ENV . '/');
-define('DIR_SYSTEM', '/var/www/html/' . $NODE_ENV . '/system/');
-define('DIR_DATABASE', '/var/www/html/' . $NODE_ENV . '/system/database/');
-define('DIR_LANGUAGE', '/var/www/html/' . $NODE_ENV . '/language/');
-define('DIR_TEMPLATE', '/var/www/html/' . $NODE_ENV . '/view/template/');
-define('DIR_CONFIG', '/var/www/html/' . $NODE_ENV . '/system/config/');
-define('DIR_IMAGE', '/var/www/html/' . $NODE_ENV . '/image/');
-define('DIR_CACHE', '/var/www/html/' . $NODE_ENV . '/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/' . $NODE_ENV . '/download/');
-define('DIR_LOGS', '/var/www/html/' . $NODE_ENV . '/system/logs/');
-define('DIR_CATALOG', '/var/www/html/' . $NODE_ENV . '/catalog/');
+define('DIR_APPLICATION', $RELPATH . '/' . $NODE_ENV . '/');
+define('DIR_SYSTEM', $RELPATH . '/' . $NODE_ENV . '/system/');
+define('DIR_DATABASE', $RELPATH . '/' . $NODE_ENV . '/system/database/');
+define('DIR_LANGUAGE', $RELPATH . '/' . $NODE_ENV . '/language/');
+define('DIR_TEMPLATE', $RELPATH . '/' . $NODE_ENV . '/view/template/');
+define('DIR_CONFIG', $RELPATH . '/' . $NODE_ENV . '/system/config/');
+define('DIR_IMAGE', $RELPATH . '/' . $NODE_ENV . '/image/');
+define('DIR_CACHE', $RELPATH . '/' . $NODE_ENV . '/system/cache/');
+define('DIR_DOWNLOAD', $RELPATH . '/' . $NODE_ENV . '/download/');
+define('DIR_LOGS', $RELPATH . '/' . $NODE_ENV . '/system/logs/');
+define('DIR_CATALOG', $RELPATH . '/' . $NODE_ENV . '/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
