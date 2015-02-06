d395d32886168f76812056812641f719cca2d08a
Shih-Min Lee <ssmlee04@Shih-Mins-MacBook-Pro.local>
2015-02-06 16:08:29 +0800

    x

--- model/setting/store.php
+++ model/setting/store.php
@@ -10,7 +10,7 @@ class ModelSettingStore extends Model {
 	}
 
 	public function editStore($store_id, $data) {
-		$this->db->query("UPDATE " . DB_PREFIX . "store SET name = '" . $this->db->escape($data['config_name']) . "', `url` = '" . $this->db->escape($data['config_url']) . "'");//, `ssl` = '" . $this->db->escape($data['config_ssl']) . "' WHERE store_id = '" . (int)$store_id . "'");
+		$this->db->query("UPDATE " . DB_PREFIX . "store SET name = '" . $this->db->escape($data['config_name']) . "', `url` = '" . $this->db->escape($data['config_url']) . "' WHERE store_id = '" . (int)$store_id . "'"); //, `ssl` = '" . $this->db->escape($data['config_ssl']) . "' WHERE store_id = '" . (int)$store_id . "'");
 
 		$this->cache->delete('store');
 	}
